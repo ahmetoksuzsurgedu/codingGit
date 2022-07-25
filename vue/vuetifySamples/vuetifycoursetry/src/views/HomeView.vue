ab61ade78c1f3295276bfe83405fe349f09b6108
ahmetoksuzsurgedu <ahmetoksuz@surgedu.com>
2022-07-25 19:11:14 +0200

    link oder linkS is a long time

---
+++ vue/vuetifySamples/vuetifycoursetry/src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <h2>Burasi try<br/> homepage</h2>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    //HelloWorld
+  }
+}
+</script>
---
+++ vue/vuetifySamples/vuetifycoursetry2/src/App.vue
@@ -0,0 +1,70 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <v-toolbar-title>Vuetify Dashboard</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <!-- <v-btn text rounded to="/">Home</v-btn>  bu ikisi yerine v-forla birini asagidaki sekle
+      <v-btn text rounded to="/login">Login</v-btn>  cevirince ayni isi yapiyorlar-->
+      <v-btn
+        v-for="link in links"
+        :key="`${link.label}-header-link`"
+        text 
+        rounded 
+        :to="link.url"
+        >
+        {{ link.label }}
+      </v-btn>
+    </v-app-bar>
+    <v-content>
+      <router-view/>
+    </v-content>
+
+    <!-- <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/login">Login</router-link>
+    </nav> -->
+    <v-footer color="primary lighten-1" padless>
+      <v-layout justify-center wrap>
+        <v-btn
+          v-for="link in links"
+          :key="`${link.label}-footer-link`"
+          color="white"
+          text
+          rounded
+          class="my-2"
+          :to="link.url"
+        >
+          {{ link.label }}
+        </v-btn>
+        <v-flex primary lighten-2 py-4 text-center white--text xs12>
+          {{ new Date().getFullYear() }} — <strong>Vuetify Dashboard</strong>
+        </v-flex>
+      </v-layout>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+    links: [
+      {
+        label: 'Home',
+        url: '/'
+      },
+      {
+        label: 'Login',
+        url: '/login'
+      },
+      {
+        label: 'Dashboard',
+        url: '/dashboard'
+      }
+    ]
+    }
+  } 
+}
+</script>
